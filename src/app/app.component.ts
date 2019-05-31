b02b2ec49c65dc63b5bce877d694e3b1c0007844
Felipe Rodrigues <lipe2013.lipao@hotmail.com>
2019-05-31 00:16:26 -0300

    Criando o método para logar e finalizando detalhes de validação de formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from "./Services/login.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CatalogoClientes';
+
+  showNavigation = false;
+
+  constructor(private loginService: LoginService){}
+
+  ngOnInit(){
+    this.loginService.show.subscribe(
+      show => this.showNavigation = show
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Pages/login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { HomeComponent } from './Pages/home/home.component';
+import {LoginService} from "./Services/login.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
